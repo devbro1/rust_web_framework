07f443da0a82fb526d29da515c5c4bee29bee1fe
Farzad Meow Khalafi <farzadk@gmail.com>
2023-12-16 12:43:45 -0500

    adding commander Branch name: master

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,16 @@ use axum::{
     Json, Router,
 };
 
-use lambda_http::{http::StatusCode, run, Error};
-
-
 #[tokio::main]
 async fn main() {
-    // initialize tracing
-    // tracing_subscriber::fmt::init();
-
-    // build our application with a route
     let routes = Router::new()
-        // `GET /` goes to `root`
         .route("/", get(root));
-        // // `POST /users` goes to `create_user`
-        // .route("/users", post(create_user));
 
-    
-    if 1 == 1 // if it is aws lambda
-    {
-        
-    
-        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-        axum::serve(listener, routes).await.unwrap();
-    }
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, routes).await.unwrap();
 
-    run(routes).await
+
+    return;
 }
 
 async fn root() -> &'static str {
