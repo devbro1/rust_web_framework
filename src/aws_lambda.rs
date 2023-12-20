ae2cbe686d8b5d8ab7b8563dc0e319b68da6a658
Farzad Meow Khalafi <farzadk@gmail.com>
2023-12-20 12:32:59 -0500

    fixed tests Branch name: master

--- src/aws_lambda.rs
+++ src/aws_lambda.rs
@@ -1,4 +1,5 @@
 mod routes;
+mod app;
 
 use lambda_http::{
     // run,
--- tests/test2.rs
+++ tests/test2.rs
@@ -1,11 +1,11 @@
 #[cfg(test)]
 mod tests {  
-    use web_framework::routes;
+    use web_framework::routes::api::get_routes;
     use ::axum_test::TestServer;
 
     #[tokio::test]
     async fn test_add() {
-        let routes = routes::api::get_routes();
+        let routes = get_routes();
         let server = TestServer::new(routes).unwrap();
 
         let response = server.get("/").await;
